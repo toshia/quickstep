e9feb9d3c6bee10aec597d7a99146f2a77c1e1dd
Toshiaki Asai <toshi.alternative@gmail.com>
2017-06-17 19:37:59 +0900

    Windowロールのmikutterコマンドを呼び出す

--- quickstep.rb
+++ quickstep.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 require_relative 'complete'
-
+require_relative 'model/command'
 Plugin.create(:quickstep) do
   command(:quickstep,
           name: 'Quick Step',
@@ -15,10 +15,29 @@ Plugin.create(:quickstep) do
     dialog.show_all
   end
 
+  intent Plugin::Quickstep::Command, label: 'mikutterコマンド' do |intent_token|
+    window = Plugin::GUI::Window.active
+    command = intent_token.model
+    event = Plugin::GUI::Event.new(:quickstep, window, [])
+    command[:exec].(event) if command[:condition] === event
+  end
+
   # URLっぽい文字列なら、それに対してintentを発行する候補を出す
   filter_quickstep_query do |query, yielder|
     if URI.regexp =~ query
       yielder << Retriever::URI!(query)
+    end
+    [query, yielder]
+  end
+
+  filter_quickstep_query do |query, yielder|
+    if !query.empty?
+      commands, = Plugin.filtering(:command, Hash.new)
+      commands.each do |command_slug, options|
+        if options[:role] == :window and (options[:name].include?(query) or command_slug.to_s.include?(query))
+          yielder << Plugin::Quickstep::Command.new(options.merge(slug: command_slug))
+        end
+      end
     end
     [query, yielder]
   end
